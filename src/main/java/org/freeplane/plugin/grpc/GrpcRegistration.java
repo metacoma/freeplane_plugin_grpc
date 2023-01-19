6e7378d6eefebd7bb4d01d1e6ee55f1e0ecfd788
bebebeko <ryabin.ss@gmail.com>
2023-01-19 17:34:58 +0100

    basic createNode implementation

--- src/main/java/org/freeplane/plugin/grpc/GrpcRegistration.java
+++ src/main/java/org/freeplane/plugin/grpc/GrpcRegistration.java
@@ -8,6 +8,13 @@ import org.freeplane.features.icon.IconController;
 import org.freeplane.features.icon.IconMouseListener;
 import org.freeplane.features.mode.ModeController;
 
+import org.freeplane.features.mode.Controller;
+import org.freeplane.features.map.MapController;
+import org.freeplane.features.map.NodeModel;
+import org.freeplane.core.util.TextUtils;
+
+//import org.freeplane.api.Node;
+
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import io.grpc.stub.StreamObserver;
@@ -40,6 +47,22 @@ public class GrpcRegistration {
 
     		@Override
     		public void sayHello(HelloRequest req, StreamObserver<HelloReply> responseObserver) {
+                        final MapController mapController = Controller.getCurrentModeController().getMapController();
+                        final NodeModel rootNode = mapController.getRootNode();
+                        //final NodeModel selectedNode = mapController.getSelectedNode();
+                        NodeModel newNode = new NodeModel(rootNode.getMap());
+                        newNode.setText(req.getName());
+                        //mapController.insertNode(newNode, rootNode, rootNode.getChildCount());
+                        mapController.insertNodeIntoWithoutUndo(newNode, rootNode);
+                        /*
+                        rootNode.insert(newNode);
+                        */
+      			HelloReply reply = HelloReply.newBuilder().setMessage("Hello " + req.getName()).build();
+      			responseObserver.onNext(reply);
+      			responseObserver.onCompleted();
+    		}
+    		@Override
+    		public void createChild(FreeplaneNode req, StreamObserver<HelloReply> responseObserver) {
       			HelloReply reply = HelloReply.newBuilder().setMessage("Hello " + req.getName()).build();
       			responseObserver.onNext(reply);
       			responseObserver.onCompleted();
