cfd26431e045845683630f7674a248ead4b84355
bebebeko <ryabin.ss@gmail.com>
2023-01-25 18:31:03 +0100

    Add Fore/Background color gRPC functions. Add parent node argument for CreateChild gRPC function

--- src/main/java/org/freeplane/plugin/grpc/GrpcRegistration.java
+++ src/main/java/org/freeplane/plugin/grpc/GrpcRegistration.java
@@ -2,6 +2,8 @@
 package org.freeplane.plugin.grpc;
 //import org.freeplane.plugin.grpc.HelloWorldServer;
 //import io.grpc.examples.helloworld;
+//
+import java.awt.Color;
 
 import org.freeplane.features.icon.IconClickedEvent;
 import org.freeplane.features.icon.IconController;
@@ -12,6 +14,8 @@ import org.freeplane.features.attribute.AttributeController;
 import org.freeplane.features.map.mindmapmode.MMapController;
 
 import org.freeplane.features.attribute.mindmapmode.MAttributeController;
+import org.freeplane.features.nodestyle.mindmapmode.MNodeStyleController;
+import org.freeplane.features.nodestyle.NodeStyleController;
 import org.freeplane.features.link.mindmapmode.MLinkController;
 import org.freeplane.features.text.mindmapmode.MTextController;
 import org.freeplane.features.text.TextController;
@@ -30,7 +34,7 @@ import org.freeplane.core.util.TextUtils;
 import org.freeplane.api.Attributes;
 import org.freeplane.core.util.Hyperlink;
 //import org.freeplane.plugin.script.FormulaUtils;
-import org.freeplane.plugin.script.FormulaUtils;
+//import org.freeplane.plugin.script.FormulaUtils;
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
@@ -71,16 +75,30 @@ public class GrpcRegistration {
                         //final MapController mapController = Controller.getCurrentModeController().getMapController();
                         final MapController mapController = Controller.getCurrentModeController().getMapController();
                         final MMapController mmapController = (MMapController) Controller.getCurrentModeController().getMapController();
-                        final NodeModel rootNode = mapController.getRootNode();
                         final MapModel map = Controller.getCurrentController().getMap();
+                        final String parentNodeId = req.getParentNodeId();
+                        final String newNodeName = req.getName();
+                        CreateChildResponse reply;
                         
-                        System.out.println("GRPC Freeplane::createChild(" + req.getName() + ")"); 
-                        NodeModel newNodeModel = mapController.newNode("test", rootNode.getMap());
-                        newNodeModel.setSide(mapController.suggestNewChildSide(rootNode, NodeModel.Side.DEFAULT));
-                        newNodeModel.createID();
-                        mmapController.insertNode(newNodeModel, rootNode, false);
+                        System.out.println("GRPC Freeplane::createChild(name: " + req.getName() + ", parent_node_id: " + req.getParentNodeId() + ")"); 
+                        NodeModel rootNode = (parentNodeId != null && parentNodeId.length() > 0) ? map.getNodeForID(parentNodeId) : mapController.getRootNode();
+                        if (parentNodeId != null && parentNodeId.length() > 0) {
+                          System.out.println("parentNodeId is not null");
+                        } else {
+                          System.out.println("parentNodeId == null");
+                        } 
+                        if (rootNode != null) {
+                          NodeModel newNodeModel = mapController.newNode(newNodeName, rootNode.getMap());
+                          newNodeModel.setSide(mapController.suggestNewChildSide(rootNode, NodeModel.Side.DEFAULT));
+                          newNodeModel.createID();
+                          mmapController.insertNode(newNodeModel, rootNode, false);
+      			  reply = CreateChildResponse.newBuilder().setNodeId(newNodeModel.getID()).setNodeText(newNodeModel.getText()).build();
+                        } else {
+                          System.out.println("GRPC Freeplane::createChild(name: " + req.getName() + ", parent_node_id: " + req.getParentNodeId() + ") root node not found"); 
+      			  reply = CreateChildResponse.newBuilder().setNodeId("-1").setNodeText("").build();
+                        } 
 
-      			CreateChildResponse reply = CreateChildResponse.newBuilder().setNodeId(newNodeModel.getID()).setNodeText(newNodeModel.getText()).build();
+
       			responseObserver.onNext(reply);
       			responseObserver.onCompleted();
     		}
@@ -176,6 +194,52 @@ public class GrpcRegistration {
       			responseObserver.onCompleted();
                 } 
 
+                @Override
+    		public void nodeColorSet(NodeColorSetRequest req, StreamObserver<NodeColorSetResponse> responseObserver) {
+                        boolean success = false;
+                        //final MLinkController mLinkController = (MLinkController) LinkController.getController();
+                        final MNodeStyleController mNodeStyleController = (MNodeStyleController) NodeStyleController.getController();
+                        final MapModel map = Controller.getCurrentController().getMap();
+                        System.out.println("GRPC Freeplane::nodeColorSet(node_id: " + req.getNodeId() + ", color:" + req.getColor() + ")"); 
+
+                        NodeModel targetNode = map.getNodeForID(req.getNodeId());
+                        if (targetNode != null) {
+                          try {
+                            //mLinkController.setLink(targetNode, new Hyperlink(uri));
+                            success = true;
+                            mNodeStyleController.setColor(targetNode, new Color(18, 18, 18, 18));
+                          } catch(Exception e) {
+                            success = false;
+                          } 
+                        }
+
+                        NodeColorSetResponse reply = NodeColorSetResponse.newBuilder().setSuccess(success).build();
+      			responseObserver.onNext(reply);
+      			responseObserver.onCompleted();
+                } 
+
+                @Override
+    		public void nodeBackgroundColorSet(NodeBackgroundColorSetRequest req, StreamObserver<NodeBackgroundColorSetResponse> responseObserver) {
+                        boolean success = false;
+                        //final MLinkController mLinkController = (MLinkController) LinkController.getController();
+                        final MNodeStyleController mNodeStyleController = (MNodeStyleController) NodeStyleController.getController();
+                        final MapModel map = Controller.getCurrentController().getMap();
+                        System.out.println("GRPC Freeplane::nodeBackgroundColorSet(node_id: " + req.getNodeId() + ", color:" + req.getColor() + ")"); 
+
+                        NodeModel targetNode = map.getNodeForID(req.getNodeId());
+                        if (targetNode != null) {
+                          try {
+                            success = true;
+                            mNodeStyleController.setBackgroundColor(targetNode, new Color(18, 18, 18, 18));
+                          } catch(Exception e) {
+                            success = false;
+                          } 
+                        }
+
+                        NodeBackgroundColorSetResponse reply = NodeBackgroundColorSetResponse.newBuilder().setSuccess(success).build();
+      			responseObserver.onNext(reply);
+      			responseObserver.onCompleted();
+                } 
   	}
 
 }
